c62e0c70d07ff5b95435818abc089bd08b54716c
zaidalderi <57761750+zaidalderi@users.noreply.github.com>
2022-03-07 23:36:50 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,34 +1,18 @@
-import 'react-native-gesture-handler';
-import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { MainStackNavigator } from "./navigation/StackNavigator";
+import 'react-native-gesture-handler'
+import React, { Component } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import DrawerNavigator from './navigation/DrawerNavigation'
+import FlashMessage from 'react-native-flash-message'
 
-import DrawerNavigator from "./navigation/DrawerNavigation";
-
-import HomeScreen from './screens/home';
-import LoginScreen from './screens/login';
-import SignupScreen from './screens/signup';
-import LogoutScreen from './screens/logout';
-import Profile from './screens/profile';
-import Friends from './screens/friends';
-import NewPost from './screens/newpost';
-import FriendSearch from './screens/friendsearch';
-import UpdateProfile from './screens/updateprofile';
-
-const Drawer = createDrawerNavigator();
-
-
-
-class App extends Component{
-    
-    render(){
-        return (
-            <NavigationContainer>
-                <DrawerNavigator/>
-            </NavigationContainer>
-        );
-    }
+class App extends Component {
+  render () {
+    return (
+      <NavigationContainer>
+        <FlashMessage position='top' icon={{ icon: 'warning' }} style={{ paddingTop: 0, backgroundColor: '#0096c7' }} />
+        <DrawerNavigator />
+      </NavigationContainer>
+    )
+  }
 }
 
-export default App;
+export default App
